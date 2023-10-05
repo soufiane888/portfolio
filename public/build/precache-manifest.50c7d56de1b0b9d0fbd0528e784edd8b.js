b129d072e63517334c8db48223847ce524201854
saddik <saddik_soufiane@hotmaim.com>
2023-10-05 21:10:21 +0200

    build

---
+++ public/build/precache-manifest.50c7d56de1b0b9d0fbd0528e784edd8b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fa079cb416a2dcd5097288e4543c14e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c073277f1fee02eb5df4",
+    "url": "/static/css/main.54bcac4b.chunk.css"
+  },
+  {
+    "revision": "139ad6e084386cc1bb10",
+    "url": "/static/js/10.cfb3fd52.chunk.js"
+  },
+  {
+    "revision": "afd25a6be1123f17297b",
+    "url": "/static/js/11.a6c0d15e.chunk.js"
+  },
+  {
+    "revision": "e9dc2739a373f8e76c03",
+    "url": "/static/js/2.f36fb507.chunk.js"
+  },
+  {
+    "revision": "d0ef5eba642f0edd857146cd0c823fd9",
+    "url": "/static/js/2.f36fb507.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1b3d06e8ab2cb569590",
+    "url": "/static/js/3.082358f0.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/3.082358f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62b2c628a5941c073c01",
+    "url": "/static/js/4.17e2d75e.chunk.js"
+  },
+  {
+    "revision": "d2c30c9b482116ec7db4",
+    "url": "/static/js/5.f84e254f.chunk.js"
+  },
+  {
+    "revision": "a1c1a99096f458ff467c",
+    "url": "/static/js/6.2680a083.chunk.js"
+  },
+  {
+    "revision": "0c205ea0bdf8b227e2cc",
+    "url": "/static/js/7.116dcc77.chunk.js"
+  },
+  {
+    "revision": "029f65e40f256019da4f",
+    "url": "/static/js/8.8780ba45.chunk.js"
+  },
+  {
+    "revision": "74567fb5673b64b938a8",
+    "url": "/static/js/9.5e94727f.chunk.js"
+  },
+  {
+    "revision": "c073277f1fee02eb5df4",
+    "url": "/static/js/main.128a3b52.chunk.js"
+  },
+  {
+    "revision": "bb1801b42c576b9537b0",
+    "url": "/static/js/runtime-main.ae95fd4c.js"
+  }
+]);
